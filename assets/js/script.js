4b96188efb7e12e1c81ecd9b39747846207087b7
Amy Snarr <amy.snarr16@gmail.com>
2022-11-25 11:03:41 -0700

    pressing 'enter' also searches for a city

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,10 @@ var currentWeather = $('#current-weather');
 var searchList = $('#search-list');
 
 var searches = [];
+var i = 0;
 
 // When you click the search button, the following happens
-function searchWeather(event) {
-    event.preventDefault();
-
+function searchWeather() {
     var srchTxt = searchText.val();
 
     if (srchTxt === '') {
@@ -28,36 +27,37 @@ function searchWeather(event) {
 
   }
 
+
 // Adds content to the previous searches section
 function addToList() {
-
     var storedSearches = JSON.parse(localStorage.getItem("searches"));
     console.log(storedSearches);
 
     searchList.innerHTML = "";
 
-    for (var i = 0; i < searches.length; i++) {
+    for (; i < searches.length; i++) {
         var search = searches[i];
 
         var li = document.createElement('li');
         li.textContent = search;
         li.setAttribute("data-index", i);
 
-        var button = document.createElement('button');
-        button.textContent = "x";
-
-        li.append(button);
         searchList.append(li);
     }
 }
 
 // Locally stores the entry in text box
-
 function storeSearches(){
-
     localStorage.setItem("searches", JSON.stringify(searches));
-
 }
 
 // Click events
 searchBtn.on('click', searchWeather)
+
+searchText.on('keypress', function(event){
+
+    var code = event.keyCode || event.which;
+    if (code==13) {
+        searchWeather()
+    }
+});
