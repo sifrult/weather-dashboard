d885ae422962f57f72736b8e42ec066814db7ab3
Amy Snarr <amy.snarr16@gmail.com>
2022-11-26 13:52:45 -0700

    added buttons back in to close previous searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 var searchText = $('#search-text');
+var city = $('.city');
 
 var searchBtn = $("#search-button");
 
@@ -8,7 +9,10 @@ var searchList = $('#search-list');
 
 
 var searches = [];
-var i = 0;
+
+
+var lat;
+var lon;
 
 // Adds content to the previous searches section
 function addToList() {
@@ -18,31 +22,52 @@ function addToList() {
         searches = storedSearches;
     }
 
-    searchList.innerHTML = "";
+    searchList.empty();
 
-    for (; i < searches.length; i++) {
+    for (var i = 0; i < searches.length; i++) {
         var search = searches[i];
 
-        var button = $('<button>');
-        button.text(search);
-        button.attr("data-index", i);
+        var li = $('<li>');
+        li.text(search);
+        li.attr('data-index', i);
 
-        searchList.append(button);
+        var button = $('<button>');
+        button.text('x');
+
+        li.append(button);
+        searchList.append(li);
+
+
     }
 
-// Code to find the lat and long of a city
-var apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + search.replace(" ", "_") + '&limit=5&appid=3081b8e03e427d8a8b2f19d6ac27558d';
+// Code to find the lat and lon of a city
+// var apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + search.replace(" ", "_") + '&limit=5&appid=3081b8e03e427d8a8b2f19d6ac27558d';
 
-fetch(apiUrl)
-    .then (function(response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data[0].lat)
-        console.log(data[0].lon)
-    })
+// fetch(apiUrl)
+//     .then (function(response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         lat = (data[0].lat);
+//         lon = (data[0].lon);
+//         city.text(data[0].name);
+
+
+// Find weather from lat and lon
+// var weatherApiUrl = 'http://api.openweathermap.org/data/2.5/forecast/daily?lat=' + lat + '&lon='+ lon + '&appid=3081b8e03e427d8a8b2f19d6ac27558d'
+
+// fetch(weatherApiUrl)
+//     .then (function(response) {
+//         return response.json();
+//     })
+//     .then(function(data) {
+//          console.log(data);
+//     })
+
+//     })
 
 }
+
 
 // Locally stores the entry in text box
 function storeSearches(){
@@ -85,3 +110,14 @@ searchText.on('keypress', function(event){
         addToList();
     }
 });
+
+searchList.on('click', function(event) {
+    var element = $(event.target);
+
+    if (element.is('button') === true) {
+        var index = element.parent().attr('data-index');
+        searches.splice(index, 1);
+        storeSearches();
+        addToList();
+    }
+})
