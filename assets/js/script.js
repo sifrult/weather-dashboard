7cbdc6c780297eabe7db3fcbbdc77957621bc970
Amy Snarr <amy.snarr16@gmail.com>
2022-11-25 19:25:20 -0700

    page reloads when you click x to close a previous search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-var searchInput = $('#search-input');
-
 var searchText = $('#search-text');
 
 var searchBtn = $("#search-button");
@@ -10,22 +8,6 @@ var searchList = $('#search-list');
 
 var searches = [];
 var i = 0;
-
-// When you click the search button, the following happens
-function searchWeather() {
-    var srchTxt = searchText.val();
-
-    if (srchTxt === '') {
-        return;
-    }
-
-    searches.push(srchTxt);
-    searchText.val('');
-
-    storeSearches();
-    addToList();
-  }
-
 // Adds content to the previous searches section
 function addToList() {
        searchList.innerHTML = "";
@@ -45,6 +27,7 @@ function addToList() {
     }
 }
 
+// This function runs when the page loads
 function init() {
     var storedSearches = JSON.parse(localStorage.getItem("searches"));
 
@@ -55,21 +38,46 @@ function init() {
     }
 
     addToList();
-
 }
+
 // Locally stores the entry in text box
 function storeSearches(){
     localStorage.setItem("searches", JSON.stringify(searches));
 }
 
-// Click events
-searchBtn.on('click', searchWeather)
+// When you click the search button, the following happens
+searchBtn.on('click', function(e) {
+    e.preventDefault();
+    var srchTxt = searchText.val();
+
+    if (srchTxt === '') {
+        return;
+    }
+
+    searches.push(srchTxt);
+    searchText.val('');
+
+    storeSearches();
+    addToList();
+  })
+
 
 // Pressing 'enter' triggers the search
 searchText.on('keypress', function(event){
     var code = event.keyCode || event.which;
     if (code==13) {
-        searchWeather()
+        event.preventDefault();
+        var srchTxt = searchText.val();
+
+        if (srchTxt === '') {
+            return;
+        }
+
+        searches.push(srchTxt);
+        searchText.val('');
+
+        storeSearches();
+        addToList();
     }
 });
 
@@ -79,10 +87,10 @@ searchList.on("click", function(event) {
     if (element.matches("button") === true) {
       var index = element.parentElement.getAttribute("data-index");
       searches.splice(index, 1);
-
+      window.location.reload();
       storeSearches();
       addToList();
-         }
+    }
   });
 
   init();
