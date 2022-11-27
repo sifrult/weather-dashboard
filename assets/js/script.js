b3dbd12d90f99599b4117ec3990a538d3ebb2e56
Amy Snarr <amy.snarr16@gmail.com>
2022-11-27 14:30:42 -0700

    displays images of future weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,6 +87,8 @@ fetch(todayWeatherUrl)
 // Find future weather from lat and lon
 var futureWeatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon='+ lon + '&appid=' + api + '&units=imperial';
 
+
+
 fetch(futureWeatherUrl)
     .then (function(response) {
         return response.json();
@@ -95,44 +97,51 @@ fetch(futureWeatherUrl)
 
         for ( var i = 0; i < data.list.length; i++) {
             var date = data.list[i].dt_txt
+
             var date1 = date.substr(0,10);
 
             var noon = date.substr(11,19);
 
             var reformatDate = dayjs(date1).format('MM/D/YYYY');
 
+
+
+
             if (reformatDate === oneDay && noon === '12:00:00') {
+                $('#one_day_icon').append('<img src="http://openweathermap.org/img/wn/' + (data.list[i].weather[0].icon) + '.png" alt="' + data.list[i].weather[0].description + '" title="' + data.list[i].weather[0].description + '">')
                 $('.one_day_temp').text('Temp: ' + data.list[i].main.temp + '°F');
                 $('.one_day_wind').text('Wind: ' + data.list[i].wind.speed + ' MPH');
                 $('.one_day_hum').text('Humidity: ' + data.list[i].main.humidity + '%');
             }
 
             if (reformatDate === twoDays && noon === '12:00:00') {
+                $('#two_days_icon').append('<img src="http://openweathermap.org/img/wn/' + (data.list[i].weather[0].icon) + '.png" alt="' + data.list[i].weather[0].description + '" title="' + data.list[i].weather[0].description + '">')
                 $('.two_days_temp').text('Temp: ' + data.list[i].main.temp + '°F');
                 $('.two_days_wind').text('Wind: ' + data.list[i].wind.speed + ' MPH');
                 $('.two_days_hum').text('Humidity: ' + data.list[i].main.humidity + '%');
             }
 
             if (reformatDate === threeDays && noon === '12:00:00') {
+                $('#three_days_icon').append('<img src="http://openweathermap.org/img/wn/' + (data.list[i].weather[0].icon) + '.png" alt="' + data.list[i].weather[0].description + '" title="' + data.list[i].weather[0].description + '">')
                 $('.three_days_temp').text('Temp: ' + data.list[i].main.temp + '°F');
                 $('.three_days_wind').text('Wind: ' + data.list[i].wind.speed + ' MPH');
                 $('.three_days_hum').text('Humidity: ' + data.list[i].main.humidity + '%');
             }
 
             if (reformatDate === fourDays && noon === '12:00:00') {
+                $('#four_days_icon').append('<img src="http://openweathermap.org/img/wn/' + (data.list[i].weather[0].icon) + '.png" alt="' + data.list[i].weather[0].description + '" title="' + data.list[i].weather[0].description + '">')
                 $('.four_days_temp').text('Temp: ' + data.list[i].main.temp + '°F');
                 $('.four_days_wind').text('Wind: ' + data.list[i].wind.speed + ' MPH');
                 $('.four_days_hum').text('Humidity: ' + data.list[i].main.humidity + '%');
             }
 
             if (reformatDate === fiveDays && noon === '12:00:00') {
+                $('#five_days_icon').append('<img src="http://openweathermap.org/img/wn/' + (data.list[i].weather[0].icon) + '.png" alt="' + data.list[i].weather[0].description + '" title="' + data.list[i].weather[0].description + '">')
                 $('.five_days_temp').text('Temp: ' + data.list[i].main.temp + '°F');
                 $('.five_days_wind').text('Wind: ' + data.list[i].wind.speed + ' MPH');
                 $('.five_days_hum').text('Humidity: ' + data.list[i].main.humidity + '%');
             }
         }
-
-
     });
     })
 
