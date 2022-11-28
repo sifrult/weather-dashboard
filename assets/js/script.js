33d64727b3dc971b3b80f6d306c698e38d246766
Amy Snarr <amy.snarr16@gmail.com>
2022-11-27 20:47:13 -0700

    can re-render a previous search

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,38 +13,11 @@ var currentWeather = $('#current-weather');
 var searchList = $('#search-list');
 
 var api = '3081b8e03e427d8a8b2f19d6ac27558d';
+
 var searches = [];
 
-
-var lat;
-var lon;
-
-// Adds content to the previous searches section
-function addToList() {
-    var storedSearches = JSON.parse(localStorage.getItem("searches"));
-
-    if (storedSearches !== null) {
-        searches = storedSearches;
-    }
-
-    searchList.empty();
-
-    for (var i = 0; i < searches.length; i++) {
-        var search = searches[i];
-
-        var li = $('<li>');
-        var link = $('<a>');
-        var button = $('<button>');
-
-        link.text(search);
-        link.attr('href', '#');
-        li.attr('data-index', i);
-        button.text('×');
-
-        li.append(link);
-        li.append(button);
-        searchList.append(li);
-    }
+// Runs the whole page
+function renderItems(search) {
 
 // Code to find the lat and lon of a city
 var apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + search.replace(" ", "_") + '&limit=5&appid=' + api;
@@ -54,8 +27,8 @@ fetch(apiUrl)
         return response.json();
     })
     .then(function (data) {
-        lat = (data[0].lat);
-        lon = (data[0].lon);
+        var lat = (data[0].lat);
+        var lon = (data[0].lon);
         city.text(data[0].name);
 
 // Display the dates
@@ -87,8 +60,6 @@ fetch(todayWeatherUrl)
 
 // Find future weather from lat and lon
 var futureWeatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon='+ lon + '&appid=' + api + '&units=imperial';
-
-
 
 fetch(futureWeatherUrl)
     .then (function(response) {
@@ -147,9 +118,7 @@ fetch(futureWeatherUrl)
         }
     });
     })
-
 }
-
 
 // Locally stores the entry in text box
 function storeSearches(){
@@ -170,8 +139,24 @@ searchBtn.on('click', function(e) {
 
     $('.hr').css('display', 'block');
 
+    searchList.empty();
+
+    for (var i = 0; i < searches.length; i++) {
+        var search = searches[i];
+
+        var li = $('<li>');
+        var link = $('<a>');
+
+        link.text(search);
+        link.attr('href', '#').attr('class', search);
+        li.attr('data-index', i);
+
+        li.append(link);
+        searchList.append(li);
+    }
+
     storeSearches();
-    addToList();
+    renderItems(search);
   })
 
 // Pressing 'enter' triggers the search
@@ -190,19 +175,30 @@ searchText.on('keypress', function(event){
 
         $('.hr').css('display', 'block');
 
+        searchList.empty();
+
+        for (var i = 0; i < searches.length; i++) {
+            var search = searches[i];
+
+            var li = $('<li>');
+            var link = $('<a>');
+
+            link.text(search);
+            link.attr('href', '#').attr('class', search);
+            li.attr('data-index', i);
+
+            li.append(link);
+            searchList.append(li);
+        }
+
         storeSearches();
-        addToList();
+        renderItems(search);
     }
 });
 
-// Close previous search
-searchList.on('click', function(event) {
-    var element = $(event.target);
+// Re-render a previous search
+$(document).on('click', 'a', function() {
+    var search = $(this).attr('class');
+    renderItems(search);
 
-    if (element.is('button') === true) {
-        var index = element.parent().attr('data-index');
-        searches.splice(index, 1);
-        storeSearches();
-        addToList();
-    }
 })
